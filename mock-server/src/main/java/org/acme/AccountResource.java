78977ce1242ebe488e872bd1dc7e0d037e4498fc
Felipe Henrique Gross Windmoller <felipewind@gmail.com>
2022-03-03 19:58:20 -0300

    update

--- mock-server/src/main/java/org/acme/AccountResource.java
+++ mock-server/src/main/java/org/acme/AccountResource.java
@@ -4,22 +4,26 @@ import java.net.URI;
 import java.util.UUID;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.jboss.logging.Logger;
 
 @Path("/account")
+@Produces(MediaType.APPLICATION_JSON)
 public class AccountResource {
 
     private static final Logger LOG = Logger.getLogger(AccountResource.class);
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @POST
     public Response create(@Context UriInfo uriInfo) {
 
         LOG.info("create()");
@@ -29,6 +33,18 @@ public class AccountResource {
         URI location = uriInfo.getAbsolutePathBuilder().path(account.id).build();
 
         return Response.created(location).entity(account).build();
+
+    }
+
+    @GET
+    @Path("{id}")
+    public Response get(@Parameter(example = "1") @PathParam("id") String id) {
+
+        LOG.info("get() " + id);
+
+        var account = new Account(id, "BankBoston");
+
+        return Response.status(Status.OK).entity(account).build();
 
     }
 }
